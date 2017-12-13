3305a46e714b661d7adb9c59eef657836fd034d7
egee-irl <brian@egee.io>
2017-12-13 13:17:28 -0800

    simplify things a bit

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sed -i s/__PASSWORD__/$MINECRAFT_PASSWORD/ server.properties
-java -Xms512M -Xmx1G -XX:MaxPermSize=128M -XX:+UseConcMarkSweepGC -jar spigot*.jar
+java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot*.jar
